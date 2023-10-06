b1a04876ba1e6c0b12ae0687d01ba947b446acc1
MatvejsAleksejevsss <matvejaleksejv@gmail.com>
2023-10-06 18:33:14 +0200

    changes

---
+++ prep-course-warm-up-main/src/32-reading-status.ts
@@ -0,0 +1,32 @@
+export {};
+
+const library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    isRead: true
+  },
+  {
+    title: "Steve Jobs",
+    author: "Walter Isaacson",
+    isRead: true
+  },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    isRead: false
+  }
+];
+
+const showStatus = () => {};
+
+showStatus(library);
+
+/*
+  Expected output:
+
+  Already read 'Bill Gates' by The Road Ahead.
+  Already read 'Steve Jobs' by Walter Isaacson.
+  You still need to read 'Mockingjay: The Final Book of The Hunger Games' by Suzanne Collins.
+
+*/
